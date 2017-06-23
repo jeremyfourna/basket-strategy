dae82033108def2a2a890dbc0aa72890e2f1921f
Jeremy Fourna <jeremy.fourna@westwing.de>
2017-06-23 09:46:39 +0200

    Improve lisibility

--- dist/script.js
+++ dist/script.js
@@ -4,9 +4,15 @@ $('#runConfiguration').on('click', function(event) {
 	const functionToLaunch = $('#offensivePlaySelector').val();
 	const sizeToDisplay = $('#offensivePlaySize').val();
 
-	if (R.and(R.equals(R.isNil(functionToLaunch), false), R.equals(R.isNil(sizeToDisplay), false))) {
+	if (
+		R.and(
+			R.equals(R.isNil(functionToLaunch), false),
+			R.equals(R.isNil(sizeToDisplay), false)
+		)
+	) {
 		// Clean the body
 		cleanSVG();
+		// Run the strategy
 		strategySelector(sizeToDisplay, functionToLaunch);
 	}
 });
