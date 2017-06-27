af4169891f7fed39b383e84392400f427aaebe02
Jeremy Fourna <jeremy.fourna@westwing.de>
2017-06-27 15:05:35 +0200

    Improve firebase file

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,5 +1,18 @@
 const firebase = require("firebase");
 const config = require('./config.js').config;
+// Create a file named config.js
+// In this file create a function named config that return an object
+// with the following properties
+// {
+//		apiKey: "",
+//		authDomain: "",
+//		databaseURL: "",
+//		projectId: "",
+//		storageBucket: "",
+//		messagingSenderId: ""
+// };
+// And export the function via
+// exports.config = config;
 
 
 function startFirebase() {
